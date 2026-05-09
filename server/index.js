67c5df7596dafb0a24ec1d2729061d1c095c599d
zhqdir <zhqdir@163.com>
2026-05-10 01:44:07 +0800

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-// 端口必须这样写，Railway 才能用
 const PORT = process.env.PORT || 3000;
 
-// 自动返回首页 → 解决 Cannot GET /
 app.get('/', (req, res) => {
-  res.send('<h1>项目启动成功！</h1>');
+  res.send('<h1>部署成功！项目正常运行!</h1>');
 });
 
-// 监听 0.0.0.0 必须写
 app.listen(PORT, '0.0.0.0', () => {
-  console.log('服务已启动');
+  console.log('Server started on port', PORT);
 });
